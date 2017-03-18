2de463923de1b8a99d1e8ed80c2645a168dda256
Smells Good <arturas.pilybas@gmail.com>
2017-03-18 18:14:38 +0200

    Add files via upload

---
+++ Assets/Scripts/ExitButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitButton : MonoBehaviour
+{
+
+    public void ExitGameButton(string rip)
+    {
+        Application.Quit();
+    }
+    
+}
